5f17779a662c9fbf831c88b9ce22c92beb5573cc
Rinciya97 <rinciyadafney@gmail.com>
2019-05-29 11:52:35 +0530

    muzixApplicationPropertiesAdded

--- muzixapplication/src/main/java/com/stackroute/muzixapplication/MuzixApplication.java
+++ muzixapplication/src/main/java/com/stackroute/muzixapplication/MuzixApplication.java
@@ -1,16 +1,32 @@
 package com.stackroute.muzixapplication;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 
 public class MuzixApplication {
+    public static void main(String[] args) {
 
-
-        public static void main(String[] args) {
-
-            SpringApplication.run(MuzixApplication.class, args);
-        }
-
+        SpringApplication.run(MuzixApplication.class, args);
     }
+}
+    @RefreshScope
+    @RestController
+    class MessageRestController {
+
+        @Value("${msg:Hello world - Config Server is not working..please check}")
+        private String msg;
+
+        @RequestMapping("/msg")
+        String getMsg() {
+            return this.msg;
+        }
+    }
+
 
